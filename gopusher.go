cf22363d24ebd48919a456c5bdf6fba4e6b8bcad
jblotus <jblotus@gmail.com>
2014-05-10 19:28:43 +0000

    added max channels error

--- gopusher.go
+++ gopusher.go
@@ -1,5 +1,16 @@
 package gopusher
 
-func Trigger() string {
-    return "this will return a pusher response eventually"
+import "errors"
+
+const (
+    MAX_PUSHER_CHANNELS = 100
+)
+
+func Trigger(channels []string) (string, error) {
+    
+    if (len(channels) > MAX_PUSHER_CHANNELS) {
+        err := errors.New("error message")
+        return "", err;
+    }
+    return "this will return a pusher response eventually", nil
 }
--- gopusher_test.go
+++ gopusher_test.go
@@ -4,11 +4,130 @@ import (
     "testing"
 )
 
-func Test_Trigger(t *testing.T) {
-    r := Trigger();
+func Test_Trigger(t *testing.T) {    
+    channels := []string{"foo", "bar", "baz"}
+    
+    r, err := Trigger(channels);
     
     if (r != "this will return a pusher response eventually") {
         t.Errorf("expected `%s` to be `foo`", r);    
+    }
+    
+    if (err != nil) {
+        t.Errorf("some error happened");
+    }
+    
+}
+
+func Test_Trigger_ErrorsOnTooManyChannels(t *testing.T) {    
+    channels := []string{
+        "Shuffletag",
+        "Vinte",
+        "Jabbercube",
+        "Edgeify",
+        "Rhycero",
+        "Buzzshare",
+        "Edgeclub",
+        "Npath",
+        "Kazio",
+        "Yadel",
+        "Thoughtbeat",
+        "Myworks",
+        "Thoughtstorm",
+        "Jayo",
+        "Camido",
+        "Photobug",
+        "Twitterlist",
+        "Layo",
+        "Eidel",
+        "Mynte",
+        "Mynte",
+        "Wikibox",
+        "Devbug",
+        "Agimba",
+        "Zooxo",
+        "Mynte",
+        "Skipstorm",
+        "Cogibox",
+        "Quatz",
+        "Ntag",
+        "Photobug",
+        "Realpoint",
+        "Buzzster",
+        "Lajo",
+        "Rhynyx",
+        "Eimbee",
+        "Devpulse",
+        "Rhybox",
+        "Voomm",
+        "Ntag",
+        "Riffwire",
+        "Izio",
+        "Aimbu",
+        "Feednation",
+        "Skyble",
+        "Realbuzz",
+        "Kwinu",
+        "Rhyloo",
+        "Edgepulse",
+        "Omba",
+        "Livepath",
+        "Yozio",
+        "Skyble",
+        "Edgewire",
+        "Meevee",
+        "Gabvine",
+        "Zoombox",
+        "Blogpad",
+        "Voolith",
+        "Avavee",
+        "Topdrive",
+        "Plambee",
+        "Jaxbean",
+        "Einti",
+        "Ntags",
+        "Skipfire",
+        "Jabbertype",
+        "Jaxworks",
+        "Topicblab",
+        "Zoonder",
+        "Tazz",
+        "Roodel",
+        "Twinte",
+        "Trilith",
+        "Meeveo",
+        "Topicblab",
+        "Browsecat",
+        "Dazzlesphere",
+        "Mita",
+        "Voomm",
+        "Minyx",
+        "Oodoo",
+        "Tazzy",
+        "Meevee",
+        "Devcast",
+        "Feedmix",
+        "Quamba",
+        "Pixonyx",
+        "Edgewire",
+        "Fiveclub",
+        "Jabbersphere",
+        "Linktype",
+        "Twitterwire",
+        "Browsedrive",
+        "Mynte",
+        "Skajo",
+        "Browseblab",
+        "Aimbo",
+        "Gigashots",
+        "Yakijo",
+        "JooJoo",
+    }
+    
+    r, err := Trigger(channels);
+    
+    if err == nil {
+       t.Errorf("expected error due to too many channels, got `%s` instead", r);
     }
     
 }
